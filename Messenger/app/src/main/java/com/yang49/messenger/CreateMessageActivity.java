73f6bdddc28b276fe7f2320180e04b03930cb1bf
Yang <liyang9249@gmail.com>
2016-06-03 20:42:16 -0400

    init

---
+++ Messenger/app/src/main/java/com/yang49/messenger/CreateMessageActivity.java
@@ -0,0 +1,16 @@
+package com.yang49.messenger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+
+    //Call onSendMessage() when the button is called
+
+}
