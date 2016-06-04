0395b0aedd4bf8995fa43901ce0eff6816d69e31
Yang <liyang9249@gmail.com>
2016-06-03 20:00:36 -0400

    add onClick

--- BeerAdviser/app/src/main/java/com/yang49/beeradviser/FindBeerActivity.java
+++ BeerAdviser/app/src/main/java/com/yang49/beeradviser/FindBeerActivity.java
@@ -2,6 +2,10 @@ package com.yang49.beeradviser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class FindBeerActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class FindBeerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+    }
+
+    //Call when the user clicks the button
+    public void onClickFindBeer(View view) {
+        //Get a reference to the TextView
+        TextView brands = (TextView) findViewById(R.id.brands);
+
+        //Get a reference to the Spinner
+        Spinner color = (Spinner) findViewById(R.id.color);
+
+        //Get the selected item in the Spinner
+        String selected = String.valueOf(color.getSelectedItem());
+
+        //Display the selected item
+        brands.setText(selected);
     }
 }
