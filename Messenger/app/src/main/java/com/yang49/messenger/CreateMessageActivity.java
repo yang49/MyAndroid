8d79eb0b9b9bab033a91be1b2c95a2a7cf1c3026
Yang <liyang9249@gmail.com>
2016-06-06 13:30:57 -0400

    add second activity

--- Messenger/app/src/main/java/com/yang49/messenger/CreateMessageActivity.java
+++ Messenger/app/src/main/java/com/yang49/messenger/CreateMessageActivity.java
@@ -1,7 +1,9 @@
 package com.yang49.messenger;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreateMessageActivity extends AppCompatActivity {
 
@@ -12,5 +14,8 @@ public class CreateMessageActivity extends AppCompatActivity {
     }
 
     //Call onSendMessage() when the button is called
-
+    public void onSendMessage(View view) {
+        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        startActivity(intent)
+    }
 }
---
+++ Messenger/app/src/main/java/com/yang49/messenger/ReceiveMessageActivity.java
@@ -0,0 +1,13 @@
+package com.yang49.messenger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReceiveMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_message);
+    }
+}
